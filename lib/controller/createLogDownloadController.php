e8540f0ee66340ca4138d5a9322653def3ce27f6
Mercedes Garcia <merc85@gmail.com>
2013-06-18 13:27:13 +0200

    background process to export log files

--- lib/controller/createLogDownloadController.php
+++ lib/controller/createLogDownloadController.php
@@ -1,20 +1,20 @@
 <?php
 
 
-class createLogDownloadController extends downloadController {
+class createLogDownloadController extends ajaxcontroller {
     
     private $id_job;
     private $id_file;
-    private $file_name;
-    private $src_lang;
+    //private $file_name;
+    //private $src_lang;
     
     public function __construct() {
         parent::__construct();
         
         $this->id_file = $this->get_from_get_post('fileid');
         $this->id_job = $this->get_from_get_post('jobid');
-        $this->file_name = $this->get_from_get_post('filename');
-        $this->src_lang = $this->get_from_get_post('srclang');
+        //$this->file_name = $this->get_from_get_post('filename');
+        //$this->src_lang = $this->get_from_get_post('srclang');
         
         log::doLog("POST: ".print_r($_POST, true));
         
@@ -42,41 +42,45 @@ class createLogDownloadController extends downloadController {
     public function doAction() {
         
         //check if id file and job is a number
-        if (is_numeric($this->id_file) && is_numeric($this->id_job))
-        {
+        //if (is_numeric($this->id_file) && is_numeric($this->id_job))
+        //convert id_file and id_job into a numbers
+        $id_file = (int)$this->id_file;
+        $id_job = (int)$this->id_job;
 
-            ini_set('max_execution_time', 6000);
+            
+        ini_set('max_execution_time', 6000);
 
-            $returnValue = -1;
+        $ret = -1;
         
-            if (substr(php_uname(), 0, 7) == "Windows"){
-                log::doLog("windows");
-                $ret = exec("C:\wamp\bin\php\php5.4.3\php ".INIT::$MODEL_ROOT."/exportLog.php ".$this->id_file." ".$this->id_job." 1", $returnValue);
-            }
-            else{
-                $ret = exec("php ".INIT::$MODEL_ROOT."/exportLog.php ".$this->id_file." ".$this->id_job." 1", $returnValue);
-            }
-        
-            log::doLog("CASMACAT: return: ".$ret);
-            //log::doLog("CASMACAT: Return Value: ".print_r($returnValue,true)); 
-        
-           
-//        $argc = 3;
-//        $argv[1] = $this->id_file;
-//        $argv[2] = $this->id_job;
-//        require INIT::$MODEL_ROOT . "/exportLog.php";
-        
-            ini_set('max_execution_time', 30);
+        if (substr(php_uname(), 0, 7) == "Windows"){
+            log::doLog("windows");
+            $ret = pclose(popen("start C:\wamp\bin\php\php5.4.3\php ".INIT::$MODEL_ROOT."/exportLog.php ".$id_file." ".$id_job." 1", "r"));
+        }
+        else{
+            $ret = pclose(popen("nohup php ".INIT::$MODEL_ROOT."/exportLog.php ".$id_file." ".$id_job." 1 &", "r"));
         }
         
-        $this->filename ="log_id".$this->id_file."_".$this->file_name.".xml";
-        header('Content-Type: text/xml; charset=UTF-8');
-        header('Content-Disposition: attachment; filename="' . $this->file_name . '.xml"');
+        log::doLog("CASMACAT: return exportLog: ".$ret);
+
+        ini_set('max_execution_time', 30);
         
-        //log::doLog("CASMACAT: file: ".INIT::$LOG_DOWNLOAD . "/" .$this->filename);
+//        $this->filename ="log_id".$this->id_file."_".$this->file_name.".xml";
+//        header('Content-Type: text/xml; charset=UTF-8');
+//        header('Content-Disposition: attachment; filename="' . $this->file_name . '.xml"');
+//        
+//        //log::doLog("CASMACAT: file: ".INIT::$LOG_DOWNLOAD . "/" .$this->filename);
+//        
+//        $this->content = file_get_contents(INIT::$LOG_DOWNLOAD . "/" . $this->filename);
         
-        $this->content = file_get_contents(INIT::$LOG_DOWNLOAD . "/" . $this->filename);
-        
+
+        if($ret == "END"){
+            $this->result['code'] = 0;
+            $this->result['data'] = "OK";
+        }
+        else{
+            $this->result['errors'] = "It is not possible to get the log file";
+            $this->result['code'] = -1;
+        }
                 
     }
     
