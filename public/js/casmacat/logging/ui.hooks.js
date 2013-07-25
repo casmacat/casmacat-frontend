casmacat/casmacat-frontend

commit 3af7ca5604125bb894e87a219e8c10b33b438a5a

Checked segment exists

public/js/casmacat/logging/ui.hooks.js
@@ -86,6 +86,7 @@ $(function(){
     var lastSegmentClosed = null;
     var original_closeSegment = UI.closeSegment;
     UI.closeSegment = function(segment, byButton) {
+        if (!segment) return false;
 //        debug("ui.hooks: closeSegment()...");
 
         if (lastSegmentClosed === null && byButton === 1) {

commit 5c9db289d42b1940e695de073564382aad32aa6c

fixed for bug introduced by commit 3af7ca5604 (two segments open at the same time)

public/js/casmacat/logging/ui.hooks.js
@@ -86,15 +86,21 @@ $(function(){
     var lastSegmentClosed = null;
     var original_closeSegment = UI.closeSegment;
     UI.closeSegment = function(segment, byButton) {
-        if (!segment) return false;
 //        debug("ui.hooks: closeSegment()...");
 
-        if (lastSegmentClosed === null && byButton === 1) {
-            lastSegmentClosed = segment.id;
+        if (!segment) { // this fixes commit 4c320792d0 (this commit breaks showing itp translation matches tab)
+            original_closeSegment.call(UI, segment, byButton);
+            return;
         }
-        original_closeSegment.call(UI, segment, byButton);
-        if (lastSegmentClosed !== null && byButton === 0) {
-            if (lastSegmentClosed === segment.id) {
+
+        if (lastSegmentClosed === null && byButton === 0) { // me: 0
+            lastSegmentClosed = segment.prop("id");
+            debug("ui.hooks: Last closed segment id: '" + lastSegmentClosed + "'.");
+        }
+//        UI.toSegment = undefined;
+        original_closeSegment.call(UI, segment[0], byButton);
+        if (lastSegmentClosed !== null && byButton === 1) { // me: 1
+            if (lastSegmentClosed === segment.prop("id")) {
                 debug("cat.js: Ignoring second closeSegment() call...");
                 lastSegmentClosed = null;
                 return;